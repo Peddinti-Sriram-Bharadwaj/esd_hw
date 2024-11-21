7b89fd7d751c41f1444dc270e3c91eaf6cd96b5d
Sriram Bharadwaj <sriram9217@gmail.com>
2024-11-21 22:27:47 +0530

    created entity Customer

---
+++ src/main/java/com/sriram9217/esdtask/entity/Customer.java
@@ -0,0 +1,28 @@
+package com.sriram9217.esdtask.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
