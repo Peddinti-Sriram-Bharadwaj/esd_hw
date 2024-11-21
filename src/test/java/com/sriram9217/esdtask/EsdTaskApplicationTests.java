5a40d9734f6da79dca1615ea8ee3f0af5975fd84
Sriram Bharadwaj <sriram9217@gmail.com>
2024-11-21 21:17:05 +0530

    "initalized the project"

---
+++ src/test/java/com/sriram9217/esdtask/EsdTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sriram9217.esdtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsdTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
